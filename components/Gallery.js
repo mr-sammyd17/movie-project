780bf785967543514c20e02ce99edc0eb5a549da
Sammy <davidakinrinwa@hotmail.co.uk>
2022-07-10 22:29:30 +0100

    Display movies 4 at a time in a gallery format

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,4 +1,4 @@
-import { MovieCards } from '../components/MovieCards'
+import { MovieCards } from './MovieCards'
 import styles from '../styles/Home.module.css'
 
 
@@ -13,7 +13,7 @@ import styles from '../styles/Home.module.css'
   // }, [])
 
 
-export const Carosel = ({movieData}) => {
+export const Gallery = ({movieData}) => {
 return (
     <div className={styles.container} class={'rounded-t-full'} style={{display: 'flex', flexDirection: 'row', alignItems: 'center' /*, justifyContent: 'space-between'*/, background: 'grey', /*flexWrap: 'wrap'*/}}>
     {movieData ? movieData.map((movie, index) => {
