a406a90a1efb6df0a91249821b745ca8b6f86898
Sammy <davidakinrinwa@hotmail.co.uk>
2022-07-14 15:49:15 +0100

    Adding code to get buttons on the screen

--- components/MovieCards.js
+++ components/MovieCards.js
@@ -1,7 +1,8 @@
+//This file is responsible for displaying the 20 movies on the screen and managing their appearence. The first <div> creates the overall box and the second creates a box per movie
 import Image from 'next/image'
 
 export const MovieCards = ({movie, index}) => {
-    const idx = index+1
+    const idx = index+1 /*This creates the number used as part of the title listing for each individual movie*/
     return (
         <div style={{ height: '500px', width: '300px', background: 'yellow', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', padding: '5px'}}>
             <h4 style = {{textAlign: 'center'}}>{idx + ' -' + ' ' + movie.fullTitle}</h4>
